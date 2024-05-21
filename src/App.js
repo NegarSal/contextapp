19a5fcc6244feaa90103af895da644e56f52fe86
Negar <negarsaljooghi@gmail.com>
2024-05-21 13:16:33 +0330

    adding Navbar & BookList components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import BookList from './components/BookList';
+import Navbar from './components/Navbar'
 
 function App() {
   return (
     <div className="App">
-
+      <Navbar />
+      <BookList />
     </div>
   );
 }
